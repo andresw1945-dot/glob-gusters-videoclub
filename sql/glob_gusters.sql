0c1526be228f5d0fca1bb9679e9c51df1bbeb769
andres <andresw1945@gmai.com>
2026-09-21 17:18:51 -0500

    fix: Correct formatting and comments in SQL script for database creation

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -1,17 +1,20 @@
 -- Base de datos   Relacional Glob_Gusters video "Gusters": unknown
 -- Autor: Andres Mateo Lugo Lopez
 -- Fecha:2024-06-10
--- Creacion de la base de datos      
-CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `glob_gusters`;       
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
 
-CREATE TABLE Nacionalidad (
+-- Eliminación de la base de datos
+-- DROP DATABASE IF EXISTS `glob_gusters`;
+
+CREATE TABLE IF NOT EXISTS Nacionalidad (
   NacionalidadID int PRIMARY KEY,
   Nombre varchar(100) NOT NULL,
   
 );
 
-CREATE TABLE if NOT EXISTS Actor(
+CREATE TABLE IF NOT EXISTS Actor (
   ActorID int PRIMARY KEY,
   Nombre varchar(100) NOT NULL,
   Sexo varchar(20) NOT NULL,
