a3584f01d2f52129f70000e6a67426dce7a0d9c8
andres <andresw1945@gmai.com>
2026-09-21 17:08:09 -0500

    feat: Add SQL script for database and table creation with formatting improvements

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -1,5 +1,20 @@
 -- Base de datos   Relacional Glob_Gusters video "Gusters": unknown
 -- Autor: Andres Mateo Lugo Lopez
 -- Fecha:2024-06-10
+-- Creacion de la base de datos      
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `glob_gusters`;       
 
--- Creacion de la base de datos
+CREATE TABLE Nacionalidad (
+  NacionalidadID int PRIMARY KEY,
+  Nombre varchar(100) NOT NULL,
+  
+);
+
+CREATE TABLE if NOT EXISTS actor(
+ NacionalidadID int PRIMARY KEY,
+  Nombre varchar(100) NOT NULL,
+  Sexo varchar(20) NOT NULL,
+  NacionalidadID int,
+  FOREIGN KEY(NacionalidadID)REFERENCES Ncionalidad(NacionalidadID)
+);
